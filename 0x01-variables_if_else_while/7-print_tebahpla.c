0baa708f996c1db3c79630767d17742c31b82b12
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-06 09:36:43 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the lowercase alphabet in reverse, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
