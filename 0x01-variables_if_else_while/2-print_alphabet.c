f6e0181fc9bddde42783fd74c493d6f9968099e7
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-06 08:43:56 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
