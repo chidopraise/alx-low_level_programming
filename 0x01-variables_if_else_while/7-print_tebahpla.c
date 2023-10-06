75f4a39afe27659af390f59b0eb7c01dec7896b8
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-06 09:38:58 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,7 +3,8 @@
 /**
  * main - Entry point
  *
- * Description: Prints the lowercase alphabet in reverse, followed by a new line.
+ * Description: Prints the lowercase alphabet in reverse,
+ * followed by a new line.
  *
  * Return: Always 0 (Success)
  */
