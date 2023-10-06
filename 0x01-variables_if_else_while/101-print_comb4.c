905d688ee63aa21777a0fcf698464a024fae59c8
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-06 11:45:13 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible different combinations of three
+ * digits, separated by ", " (comma and space), in ascending order.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit1, digit2, digit3;
+
+	for (digit1 = 0; digit1 <= 7; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 <= 8; digit2++)
+		{
+			for (digit3 = digit2 + 1; digit3 <= 9; digit3++)
+			{
+				putchar(digit1 + '0');
+				putchar(digit2 + '0');
+				putchar(digit3 + '0');
+
+				if (digit1 != 7 || digit2 != 8 || digit3 != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
