ab3cb6d1734138e6f19303666ede87f51a8f1d9c
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-10 14:36:32 -0700

    Printing _puthar 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - check code
+ *
+ * Return: Always (0)
+ *
+ */
+
+int main(void)
+{
+	putchar("_putchar");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This Is a directory for functions and nested loops projects.
