aed70affe1d4f24e70db88ecdbbf5dce06717024
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-11 05:42:31 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,7 +1,11 @@
+#include <stdio.h>
+
 /**
  * main - check code
  *
  * Description: here we write functions that run our programmes
  */
-
-void _putchar();
+void _putchar()
+{
+	putchar("_putchar");
+}
