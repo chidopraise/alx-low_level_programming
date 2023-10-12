95adbd76f90ff3f994d8994f1ce1c17eeebd667f
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-12 07:25:41 -0700

    #include <stdio.h>
    /**
     * main - Entry point of the program
     *
     * Return: Always 0 (Success)
     */
    int main(void)
    {
        int i, sum = 0;
    
        for (i = 3; i < 1024; i++)
        {
            if (i % 3 == 0 || i % 5 == 0)
            {
                sum += i;
            }
        }
    
        printf("%d\n", sum);
    
        return (0);

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 3; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
