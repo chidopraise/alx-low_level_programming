b5a5fb7e2ea1fb211633107164655571c722bcdf
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-12 05:55:10 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table, starting with 0.
+ */
+
+void times_table(void)
+{
+	int row, column, result;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			result = row * column;
+
+			if (column != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+			if (result < 10)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((result / 10) + '0');
+			}
+
+			_putchar((result % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
