7003b1b5a400e3625239b86225f6f275b4ba9f78
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-13 07:11:52 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers 0 to 9 followed by a new line
+ *
+ */
+
+void print_numbers(void)
+{
+	int number = 0;
+
+	while (number <= 9)
+	{
+		_putchar(number + '0');
+		number++;
+	}
+
+	_putchar('\n');
+}
