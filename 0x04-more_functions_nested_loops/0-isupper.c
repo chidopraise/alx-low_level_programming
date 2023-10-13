2ad68de01fa3b7ee9146eaf69ce8a62668634c50
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-13 06:31:27 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -11,10 +11,10 @@ int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 	{
-		return 1;  /*Uppercase character*/
+		return 1;/*Uppercase character*/
 	}
 	else
 	{
-		return (0);  /*Not an uppercase character*/
+		return (0);/*Not an uppercase character*/
 	}
 }
