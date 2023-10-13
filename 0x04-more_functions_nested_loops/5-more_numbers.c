ae33658de4a62c584fa277bdc4ef5cf22d666273
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-13 07:58:12 -0700

    vi 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints numbers from 0 to 14, repeated 10 times each
+ */
+
+void more_numbers(void)
+{
+	int num, count;
+
+	for (count = 0; count < 10; count++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+			{
+				_putchar((num / 10) + '0');
+			}
+			_putchar((num % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
