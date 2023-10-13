bf64a62b39c61aa9597815893a9fb8798968ea28
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-13 09:15:57 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * largest_prime_factor - Finds the largest
+ * prime factor of a number
+ * @n: The number to find the largest prime factor of
+ *
+ * Return: The largest prime factor
+ */
+
+long largest_prime_factor(long n)
+{
+	long factor = 2;
+	while (n > factor)
+	{
+		if (n % factor == 0)
+		{
+			n = n / factor;
+		}
+		else
+		{
+			factor++;
+		}
+	}
+	return factor;
+}
+
+int main(void)
+{
+	long number = 612852475143;
+	long largest = largest_prime_factor(number);
+
+	printf("%ld\n", largest);
+	return (0);
+}
