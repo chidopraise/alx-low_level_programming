d7f0b4d8c4fce6d86d956319e69137983a0d2fd3
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-13 09:36:53 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer
+ * @n: The integer to print
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+
+	_putchar((n % 10) + '0');
+}
