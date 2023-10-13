d47a9da70c31739bc708f2003390ca82b73321c1
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-13 08:54:19 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz_1.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz_1.c
@@ -12,26 +12,21 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("Fizz");
-			if (i % 5 == 0)
-			{
-				printf("Buzz");
-			}
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
 		else
 		{
-			printf("%d", i);
-		}
-
-		if (i < 100)
-		{
-			printf(" ");
+			printf("%d ", i);
 		}
 	}
 
