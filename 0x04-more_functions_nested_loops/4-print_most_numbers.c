8d4f599fe7d50adc16b60f9f121ac7505b992ed5
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-13 07:22:13 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints numbers from 0 to 9 (excluding 2 and 4) and a new line
+ */
+
+void print_most_numbers(void)
+{
+	int number = 0;
+
+	while (number <= 9)
+	{
+		if (number != 2 && number != 4)
+		{
+			_putchar(number + '0');
+		}
+		number++;
+	}
+
+	_putchar('\n');
+}
