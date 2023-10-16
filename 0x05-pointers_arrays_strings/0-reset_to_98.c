3b370754e34000ddaca37be2b2bd0ded294b01e2
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-16 08:54:27 -0700

    void reset_to_98(int *n)

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Updates the value pointed to by a pointer to 98
+ * @n: A pointer to an int
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains files on learnings and study about pointers, array and string.
