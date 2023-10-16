51b6abd615ae631ddd7fc9cb736aad0d4d7388b1
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-16 15:50:00 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>  // Include the string.h header for strcmp
+
+#define MAX_PASSWORD_LENGTH 64
 
 /**
  * generatePassword - Generates a random password
@@ -14,7 +17,8 @@ void generatePassword(char *password, int length)
 	const int charsetSize = sizeof(charset) - 1;
 
 	int i;
-	for (i = 0; i < length; i++) {
+	for (i = 0; i < length; i++)
+	{
 		int index = rand() % charsetSize;
 		password[i] = charset[index];
 	}
@@ -22,21 +26,22 @@ void generatePassword(char *password, int length)
 	password[length] = '\0';/*Null-terminate the string*/
 }
 
-int main()
-{
+int main() {
 	int passwordLength = 15;/*Change this to match the required password length*/
-	char password[passwordLength + 1];
+	char password[MAX_PASSWORD_LENGTH];
 
-	/*Initialize the random number generato*/
+	/*Initialize the random number generator*/
 	srand((unsigned int)time(NULL));
 
 	generatePassword(password, passwordLength);
 
 	printf("Generated Password: %s\n", password);
 
-	if (strcmp(password, "Tada! Congrats") == 0) {
+	if (strcmp(password, "Tada! Congrats") == 0)
+	{
 		printf("Correct password!\n");
-	} else
+	}
+	else
 	{
 		printf("Wrong password\n");
 	}
