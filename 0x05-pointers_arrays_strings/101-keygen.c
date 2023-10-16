a0dd02bdd331135e6e63efe8605ab4400f8d2c1f
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-16 14:54:05 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define MAX_PASSWORD_LENGTH 64
+
 /**
  * generatePassword - Generates a random password
  * @password: The buffer to store the generated password
@@ -13,10 +15,8 @@ void generatePassword(char *password, int length)
 	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 	const int charsetSize = sizeof(charset) - 1;
 
-	/*Initialize the random number generator*/
-	srand((unsigned int)time(NULL));
-
-	for (int i = 0; i < length; i++)
+	int i;
+	for (i = 0; i < length; i++)
 	{
 		int index = rand() % charsetSize;
 		password[i] = charset[index];
@@ -28,7 +28,7 @@ void generatePassword(char *password, int length)
 int main()
 {
 	int passwordLength = 12;/*Change this to match your desired password length*/
-	char password[passwordLength + 1];
+	char password[MAX_PASSWORD_LENGTH];
 
 	generatePassword(password, passwordLength);
 
