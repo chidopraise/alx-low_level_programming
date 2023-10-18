d8d1ee80983786064bc8be1cb620ffbe68dc42da
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-18 15:53:00 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * _strcat - Concatenates two strings
+ * @dest: The destination string
+ * @src: The source string to be appended
+ *
+ * Return: A pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int dest_len = 0;
+	int src_len = 0;
+
+	/*Find the length of the destination string*/
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+
+	/*Find the length of the source string*/
+	while (src[src_len] != '\0')
+	{
+		src_len++;
+	}
+
+	/*Append the source string to the destination string*/
+	for (int i = 0; i < src_len; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
+
+	/*Null-terminate the resulting string*/
+	dest[dest_len + src_len] = '\0';
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory furder explains more about pointers, arrays and strings in more elaborate manner.
