e687239995b508d15cfbc986ec7f89af4da12406
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-19 05:13:58 -0700

    104-print_buffer.c

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+/**
+ * print_buffer - Prints a buffer in a specified format
+ * @b: Pointer to the buffer
+ * @size: Size of the buffer
+ */
+
+void print_buffer(char *b, int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+			{
+				printf("%02x", b[i + j]);
+			}
+			else
+			{
+				printf("  ");
+			}
+
+			if (j % 2 == 1)
+				printf(" ");
+
+			if (j == 9)
+				printf(" ");
+		}
+
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+			{
+				if (b[i + j] >= 32 && b[i + j] <= 126)
+				{
+					putchar(b[i + j]);
+				}
+				else
+				{
+					putchar('.');
+				}
+			}
+		}
+
+		printf("\n");
+	}
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,3 +10,4 @@ char *leet(char *str);
 char *rot13(char *str);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
