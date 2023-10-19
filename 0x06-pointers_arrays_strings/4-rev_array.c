b99b9a678c6714ee2cbd32b560ea3f96c12bfd91
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-18 17:27:08 -0700

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+/**
+ * reverse_array - Reverses the content of an array of integers
+ * @a: The array of integers to be reversed
+ * @n: The number of elements in the array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int start = 0;
+	int end = n - 1;
+
+	while (start < end)
+	{
+		int temp = a[start];
+		a[start] = a[end];
+		a[end] = temp;
+
+		start++;
+		end--;
+	}
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,3 +3,4 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, const char *src, int n);
 int _strcmp(const char *s1, const char *s2);
+void reverse_array(int *a, int n);
