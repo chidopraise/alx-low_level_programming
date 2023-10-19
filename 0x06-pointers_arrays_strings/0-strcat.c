4e41affa9b00cfc03c71c473d1b9931968b6f12f
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-19 14:29:17 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,7 @@
-#include "main.h"
-
 /**
  * _strcat - Concatenates two strings
  * @dest: The destination string
- * @src: The source string to be appended
+ * @src: The source string to append
  *
  * Return: A pointer to the resulting string dest
  */
@@ -13,25 +11,17 @@ char *_strcat(char *dest, char *src)
 	int dest_len = 0;
 	int src_len = 0;
 
-	/*Find the length of the destination string*/
 	while (dest[dest_len] != '\0')
 	{
 		dest_len++;
 	}
 
-	/*Find the length of the source string*/
 	while (src[src_len] != '\0')
 	{
+		dest[dest_len + src_len] = src[src_len];
 		src_len++;
 	}
 
-	/*Append the source string to the destination string*/
-	for (int i = 0; i < src_len; i++)
-	{
-		dest[dest_len + i] = src[i];
-	}
-
-	/*Null-terminate the resulting string*/
 	dest[dest_len + src_len] = '\0';
 
 	return (dest);
