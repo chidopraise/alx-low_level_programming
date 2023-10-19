4e1bc5980231cc363528eb6bcb587e88f9171054
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-19 03:31:40 -0700

    void print_number(int n)

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - Prints an integer
+ * @n: The integer to be printed
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+
+	_putchar(n % 10 + '0');
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,3 +8,4 @@ char *string_toupper(char *str);
 char *cap_string(char *str);
 char *leet(char *str);
 char *rot13(char *str);
+void print_number(int n);
