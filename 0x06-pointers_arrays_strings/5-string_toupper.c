3f5f3e3eab183072d09b2d8dd6935da123d1b525
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-18 17:47:58 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+/**
+ * string_toupper - Converts all lowercase letters in a string to uppercase
+ * @str: The input string
+ *
+ * Return: A pointer to the modified string
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - ('a' - 'A');
+		}
+		i++;
+	}
+
+	return (str);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,3 +4,4 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, const char *src, int n);
 int _strcmp(const char *s1, const char *s2);
 void reverse_array(int *a, int n);
+char *string_toupper(char *str);
