0ed04f50aa0ac7d6b1c4c73d5adf27906a4753fa
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-19 14:38:51 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,9 +1,8 @@
 /**
- * _strncat - Concatenates two strings,
- * using at most n bytes from src
+ * _strncat - Concatenates two strings up to n bytes
  * @dest: The destination string
- * @src: The source string to be appended
- * @n: The maximum number of bytes from src to use
+ * @src: The source string to append
+ * @n: The maximum number of bytes to append from src
  *
  * Return: A pointer to the resulting string dest
  */
@@ -11,21 +10,19 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int dest_len = 0;
+	int i;
 
-	/*Find the length of the destination string*/
 	while (dest[dest_len] != '\0')
 	{
 		dest_len++;
 	}
 
-	/*Append at most n bytes from the source string*/
-	for (int i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[dest_len + i] = src[i];
 	}
 
-	/*Null-terminate the resulting string*/
-	dest[dest_len + n] = '\0';
+	dest[dest_len + i] = '\0';
 
-	return (dest);
+	return dest;
 }
