c473ab6b99ccd00799b88c0bb9659be7e53e82fd
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-19 02:44:23 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,4 +6,4 @@ int _strcmp(const char *s1, const char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *str);
-char *leet(char *);
+char *leet(char *str);
