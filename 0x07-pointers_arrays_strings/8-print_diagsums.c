1c71d0ecca62b31854c13e37ef2f681f36c964ac
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-23 15:14:39 -0700

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_diagsums - Prints the sum of the diagonals in a square matrix
+ * @a: The square matrix
+ * @size: The size of the matrix
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, diag1 = 0, diag2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		diag1 += a[i * size + i];
+		diag2 += a[i * size + (size - i - 1)];
+	}
+
+	printf("%d, %d\n", diag1, diag2);
+}
