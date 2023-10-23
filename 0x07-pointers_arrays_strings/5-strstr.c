80a84cb6a9eb85d1c9c63ad4f8b9654a1d8da2ff
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-23 14:53:38 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+/**
+ * _strstr - Locates a substring in a string
+ * @haystack: The string to search in
+ * @needle: The substring to search for
+ *
+ * Return: A pointer to the beginning of the located substring, or NULL if
+ * the substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		char *start = haystack;
+		char *sub = needle;
+
+		while (*sub && *haystack == *sub)
+		{
+			haystack++;
+			sub++;
+		}
+
+		if (*sub == '\0')
+			return (start);
+
+		haystack = start + 1;
+	}
+
+	return (NULL);
+}
