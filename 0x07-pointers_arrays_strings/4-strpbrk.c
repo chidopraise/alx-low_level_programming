5350ac530d782ef13a585e0b6325cc14aaa51e98
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-23 13:55:37 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,14 +11,15 @@ char *_strpbrk(char *s, char *accept)
 	while (*s)
 	{
 		char *a = accept;
+
 		while (*a)
 		{
 			if (*s == *a)
-				return s;
+				return (s);
 			a++;
 		}
 		s++;
 	}
 
-	return (NULL);
+	return NULL;
 }
