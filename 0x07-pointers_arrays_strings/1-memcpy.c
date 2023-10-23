7eb41b7f26e2a4d04bf1bdf65e8e7869d2c8a868
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-23 12:59:02 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,15 +1,15 @@
 /**
-* _memcpy - function copies @n bytes from memory area @src
-* to memory area @dest
-*
-* @n: function copies
-*
-* @src: bytes from memory area
-*
-* @dest: to memory area
-*
-* Return: a pointer to @dest
-*/
+ *  _memcpy - function copies @n bytes from memory areac
+ *  @src to memory area @dest
+ *
+ * @n: function copies
+ *
+ * @src: bytes from memory area
+ *
+ * @dest: to memory area
+ *
+ * Return: a pointer to @dest
+ */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
