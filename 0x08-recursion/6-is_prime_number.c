aae9f765c27b689dccc9b06007e8ee349c543e7f
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-25 07:35:39 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+/**
+ * is_prime_number - Checks if a number is a prime number
+ * @n: The number to check
+ *
+ * Return: 1 if n is a prime number, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	if (n <= 3)
+	{
+		return (1);
+	}
+	if (n % 2 == 0 || n % 3 == 0)
+	{
+		return (0);
+	}
+
+	for (int i = 5; i * i <= n; i += 6)
+	{
+		if (n % i == 0 || n % (i + 2) == 0)
+		{
+			return (0);
+		}
+	}
+
+	return (1);
+}
