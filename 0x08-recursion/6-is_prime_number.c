05fa4d08fc78b6f17efaf540d6c447a501d596e0
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-25 07:53:02 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,12 +8,12 @@
 int is_prime_recursive(int n, int divisor)
 {
 	if (n < 2)
-		return 0;
+		return (0);
 	if (divisor == 1)
-		return 1;
+		return (1);
 	if (n % divisor == 0)
-		return 0;
-	return is_prime_recursive(n, divisor - 1);
+		return (0);
+	return (is_prime_recursive(n, divisor - 1));
 }
 
 /**
@@ -25,6 +25,6 @@ int is_prime_recursive(int n, int divisor)
 int is_prime_number(int n)
 {
 	if (n <= 1)
-		return 0;
-	return is_prime_recursive(n, n - 1);
+		return (0);
+	return (is_prime_recursive(n, n - 1));
 }
