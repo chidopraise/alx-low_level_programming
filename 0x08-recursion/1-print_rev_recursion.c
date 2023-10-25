11cc3f7c427d707826102e83d69aa09eac8373c9
Abraham Livinus <abrahamlivinus@gmail.com>
2023-10-25 06:33:23 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: The string to print
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
