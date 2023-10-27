2fef9858ad777a7a313326e928e2e76fb9f03b0f
Abraham <abrahamlivinus@gmail.com>
2023-10-27 16:41:38 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,15 +4,12 @@
 /**
  * main - Entry point
  * @argc: The number of command-line arguments
- * @argv: An array of command-line arguments
- *
- * Return: Always 0
+ * @argv: An array of strings containing the command-line arguments
+ * Return: 0
  */
-
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	_putchar((argc - 1) + '0');
-	_putchar('\n');
-
+	printf("%d\n", argc - 1);  /* Subtract 1 to exclude the program name itself */
+	
 	return (0);
 }
