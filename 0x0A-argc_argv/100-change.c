29267736e50fda5c65dcf5f7f66cfd6cd7b3ad02
Abraham <abrahamlivinus@gmail.com>
2023-10-27 18:44:17 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,16 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * main - prints the name of the program
- * @argc: number of arguments
- * @argv: array of arguments*
- *
- * Return: Always 0 (Success)
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array of strings containing the command-line arguments
+ * Return: 0 for success
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", *argv);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return 1;
+	}
 
-	return (1);
+	int cents = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+		printf("0\n");
+		return 0;
+	}
+
+	int coins[] = {25, 10, 5, 2, 1};
+	int num_coins = sizeof(coins) / sizeof(coins[0]);
+	int count = 0;
+
+	for (int i = 0; i < num_coins; i++)
+	{
+		while (cents >= coins[i])
+		{
+			cents -= coins[i];
+			count++;
+		}
+	}
+
+	printf("%d\n", count);
+
+	return 0;
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,5 +3,6 @@
 
 int _putchar(char c);
 int _atoi(char *s);
+int main(int argc, char *argv[]) __attribute__((unused));
 
 #endif
