3a55fdf0257b7fe0f03f291c546cbbd27b2b2d91
Abraham <abrahamlivinus@gmail.com>
2023-10-27 18:09:27 -0700

    vi 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
  * main - Entry point
  * @argc: The number of command-line arguments
  * @argv: An array of strings containing the command-line arguments
- * Return: 0 for success
+ *
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
