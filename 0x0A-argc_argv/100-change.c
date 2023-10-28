6ba8060c9a02a28810161ad3d928753189be842b
Abraham <abrahamlivinus@gmail.com>
2023-10-27 19:06:02 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,7 +10,34 @@
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n",*argv);
+	printf("%s\n", *argv);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	return (0);
 }
