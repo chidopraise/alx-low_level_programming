b1f952c9387c5d11e07ca5ac6d63dbac7e1589cd
Abraham <abrahamlivinus@gmail.com>
2023-10-27 18:49:23 -0700

    more

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,34 +10,5 @@
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return 1;
-	}
-
-	int cents = atoi(argv[1]);
-
-	if (cents < 0)
-	{
-		printf("0\n");
-		return 0;
-	}
-
-	int coins[] = {25, 10, 5, 2, 1};
-	int num_coins = sizeof(coins) / sizeof(coins[0]);
-	int count = 0;
-
-	for (int i = 0; i < num_coins; i++)
-	{
-		while (cents >= coins[i])
-		{
-			cents -= coins[i];
-			count++;
-		}
-	}
-
-	printf("%d\n", count);
-
-	return 0;
+	return (0);
 }
