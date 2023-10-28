2e6ad8d3f2e3c2d275c78d1b5cfbb1478a8eaa22
Abraham <abrahamlivinus@gmail.com>
2023-10-27 18:19:29 -0700

    -

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -6,6 +6,12 @@
  * @argc: number of arguments
  * @argv: array of arguments
  *
+ *
+ *
+ *
+ *
+ *
+ *
  * Return: Always 0 (Success)
  */
 int main(int argc __attribute__((unused)), char *argv[])
