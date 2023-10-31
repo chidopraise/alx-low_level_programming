e0e28d6474fc131a35d815cc3984411c94113999
Abraham <abrahamlivinus@gmail.com>
2023-10-31 15:49:29 -0700

    int coins[] = {25, 10, 5, 2, 1};         int num_coins = 0;

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,43 +1,40 @@
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
 
 /**
- * main - prints the name of the program
+ * main - calculates minimum number of coins for change
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: Always 0 (Success)
+ * Return: 0 for success, 1 for error
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", *argv);
+	int cents = atoi(argv[1]);
+	int coins[] = {25, 10, 5, 2, 1};
+	int num_coins = 0;
+	int i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
 
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
 
+	for (i = 0; i < 5; i++)
+	{
+		num_coins += cents / coins[i];
+		cents %= coins[i];
+	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	printf("%d\n", num_coins);
 
 	return (0);
 }
