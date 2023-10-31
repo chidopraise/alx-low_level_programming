b5c279d2a05e597a40cc09248fbd6d7a8489fba4
Abraham <abrahamlivinus@gmail.com>
2023-10-31 05:00:36 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 
 /**
- * is_space - Checks if a character is a space, tab, or newline
- * @c: The character to check
- * Return: 1 if it's a space, tab, or newline; 0 otherwise.
+ * is_space - Check if a character is a whitespace character.
+ * @c: The character to check.
+ *
+ * Return: 1 if the character is a whitespace character, 0 otherwise.
  */
 int is_space(char c)
 {
@@ -11,54 +12,48 @@ int is_space(char c)
 }
 
 /**
- * count_words - Counts the number of words in a string
- * @str: The input string
+ * count_words - Count the number of words in a string.
+ * @str: The string to count words in.
  *
  * Return: The number of words in the string.
  */
 int count_words(char *str)
 {
-	int count = 0;
+	int word_count = 0;
 	int in_word = 0;
 
 	while (*str)
 	{
-		if (is_space(*str))
+		if (!is_space(*str))
+		{
+			if (!in_word)
+			{
+				word_count++;
+				in_word = 1;
+			}
+		}
+		else
 		{
 			in_word = 0;
-		}
-		else if (in_word == 0)
-		{
-			in_word = 1;
-			count++;
 		}
 		str++;
 	}
 
-	return (count);
+	return word_count;
 }
 
 /**
- * strtow - Splits a string into words
- * @str: The input string
+ * extract_words - Extract words from a string.
+ * @str: The string to extract words from.
+ * @words: An array to store the extracted words.
  *
- * Return: A pointer to an array of strings (words) or NULL on failure.
+ * Return: 1 on success, 0 on failure.
  */
-char **strtow(char *str)
+int extract_words(char *str, char **words)
 {
-	int i, j, len = 0, in_word = 0, word_count;
-	char **words;
+	int i = 0, int j, int len, int in_word = 0, int word_count = count_words(str);
 
-	if (str == NULL || *str == '\0')
-		return (NULL);
-
-	word_count = count_words(str);
-	words = malloc(sizeof(char *) * (word_count + 1));
-
-	if (words == NULL)
-		return (NULL);
-
-	for (i = 0; i < word_count; i++)
+	while (i < word_count)
 	{
 		while (is_space(*str))
 			str++;
@@ -80,8 +75,7 @@ char **strtow(char *str)
 			{
 				for (j = i - 1; j >= 0; j--)
 					free(words[j]);
-				free(words);
-				return (NULL);
+				return 0;
 			}
 
 			for (j = 0; j < len; j++)
@@ -89,9 +83,38 @@ char **strtow(char *str)
 
 			words[i][j] = '\0';
 			str += len;
+			i++;
+		}
+		else
+		{
+			str++;
 		}
 	}
+	return 1;
+}
+
+/**
+ * strtow - Split a string into words.
+ * @str: The string to split into words.
+ *
+ * Return: An array of strings (words), or NULL on failure.
+ */
+char **strtow(char *str)
+{
+	int word_count = count_words(str);
+	if (word_count == 0)
+		return NULL;
+
+	char **words = malloc((word_count + 1) * sizeof(char *));
+	if (words == NULL)
+		return NULL;
 
 	words[word_count] = NULL;
-	return (words);
+	if (!extract_words(str, words))
+	{
+		free(words);
+		return NULL;
+	}
+
+	return words;
 }
