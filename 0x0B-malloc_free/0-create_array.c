24e108646df192cf907dece80ce400a69545fbd9
Abraham <abrahamlivinus@gmail.com>
2023-10-30 20:06:06 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,4 +1,6 @@
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * create_array - creates an array of chars and
@@ -18,7 +20,9 @@ char *create_array(unsigned int size, char c)
 	if (array == NULL)
 		return (NULL); /* Memory allocation failed */
 
-	for (unsigned int i = 0; i < size; i++)
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
 		array[i] = c;
 
 	return (array);
