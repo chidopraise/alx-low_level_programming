ed827ce1f55f61ddef2d44ecf6b700307474decd
Abraham <abrahamlivinus@gmail.com>
2023-11-06 13:22:16 -0800

    dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+This directory contains files and learnings on structures ie struct , data types
+and also how to define a custome data type for a variable or function.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Represents information about a dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
