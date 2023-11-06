cd53954e3116d5c49842c9e93058823c67858e7e
Abraham <abrahamlivinus@gmail.com>
2023-11-06 11:14:02 -0800

    0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef 0_OBJECT_LIKE_MACRO_H
-#define 0_OBJECT_LIKE_MACRO_H
+#ifndef _0_OBJECT_LIKE_MACRO_H
+#define _0_OBJECT_LIKE_MACRO_H
 
 #define SIZE 1024
 
-#endif /*0-OBJECT_LIKE_MACRO_H */
+#endif /* _0_OBJECT_LIKE_MACRO_H */
