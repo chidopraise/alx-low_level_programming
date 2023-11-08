5ff4c22966565d226d37956b5e8b70d8324741db
Abraham <abrahamlivinus@gmail.com>
2023-11-08 07:44:41 -0800

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * print_name - this function prints names passed through
+ * the parameter.
+ *
+ * @name: is a variable pointer that collects names to be printed
+ * @f: is a function pointer that points to a function
+ * RETURN: it returns Null or the name in the
+ * parameter
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+	{
+		return;
+	}
+
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+This directory contains files and learnings on c - function pointers.
