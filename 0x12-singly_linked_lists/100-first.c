43573108d9ea49ef0c59fc1e77d898253b66fc4c
chidopraise <abrahamlivinus@gmail.com>
2023-11-16 09:05:42 -0800

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,13 +10,3 @@ void before_main(void)
 	printf("You're beat! and yet, you must allow,
 			\nI bore my house upon my back!\n");
 }
-
-/**
- * main - The main function
- * Return: 0 on success
- */
-int main(void)
-{
-	/* Your main function logic goes here */
-	return (0);
-}
