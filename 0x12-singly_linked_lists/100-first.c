434faec29ef2cad730e67d6d5a69199d18a073ea
chidopraise <abrahamlivinus@gmail.com>
2023-11-16 09:15:07 -0800

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@ void before_main(void) __attribute__((constructor));
  */
 void before_main(void)
 {
-	printf("You're beat! and yet, you must allow,
-			\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back");
+	printf("!\n");
 }
