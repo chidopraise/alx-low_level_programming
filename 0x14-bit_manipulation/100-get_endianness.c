b78ed405ea14484f46e1712d9b258c3f8ebfc150
chidopraise <abrahamlivinus@gmail.com>
2023-11-23 10:46:26 -0800

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * get_endianness - Checks the endianness of the system.
+ *
+ * Return: 0 if big endian, 1 if little endian.
+ */
+int get_endianness(void)
+{
+	union
+	{
+		int i; /* Integer to check endianness */
+		char c[sizeof(int)]; /* Array of characters to check endianness */
+	} check_endian;
+
+	check_endian.i = 1; /* Set the integer to 1 */
+
+	/* If the first byte of the character array is 1, it's little endian */
+	return ((check_endian.c[0] == 1) ? 1 : 0);
+}
