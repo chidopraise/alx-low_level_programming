4dab2d37ba20a948f497e1d74f77b9b2a9f1325e
chidopraise <abrahamlivinus@gmail.com>
2023-11-23 09:36:05 -0800

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * binary_to_uint - Converts a binary number
+ * to an unsigned int.
+ * @b: Pointer to a string of 0 and 1 chars.
+ *
+ * Return: The converted number, or 0 if there
+ * are invalid chars or if b is NULL.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	if (b == NULL)
+		return (0);
+
+	unsigned int result = 0;
+
+	while (*b != '\0')
+	{
+		if (*b == '0' || *b == '1')
+		{
+			result = result * 2 + (*b - '0');
+			b++;
+		}
+		else
+		{
+			return (0); /* Invalid character encountered */
+		}
+	}
+
+	return (result);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,2 @@
+This directory contains files, projects and learnings on bit, bitwise operator and bit manupulations
+It also has all my findings on bit manipulations.
