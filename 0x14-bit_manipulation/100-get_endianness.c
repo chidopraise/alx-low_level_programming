e64390b20556f696279f34db522dab99ecbc009e
chidopraise <abrahamlivinus@gmail.com>
2023-11-24 06:33:38 -0800

    100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,22 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * get_endianness - Checks the endianness of the system.
  *
- * UNION - union brings together a collection of variables.
- *
- * Return: 0 if big endian, 1 if little endian.
+ * Return: 0 if the system is big endian, 1 if it's little endian.
  */
 int get_endianness(void)
 {
-	union
-	{
-		int i; /* Integer to check endianness */
-		char c[sizeof(int)]; /* Array of characters to check endianness */
-	} check_endian;
+	unsigned int x = 1;
+	char *c = (char *)&x;
 
-	check_endian.i = 1; /* Set the integer to 1 */
-
-	/* If the first byte of the character array is 1, it's little endian */
-	return ((check_endian.c[0] == 1) ? 1 : 0);
+	/* Dereference the pointer to the char array to access the byte */
+	/* and return the value indicating the endianness. */
+	return (*c);
 }
