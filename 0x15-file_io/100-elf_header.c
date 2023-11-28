f1101f9b485e327779563224daeeca49af28827f
chidopraise <abrahamlivinus@gmail.com>
2023-11-27 17:42:08 -0800

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -43,6 +43,7 @@ int main(int argc, char *argv[])
 	}
 
 	Elf64_Ehdr elf_header;
+
 	if (read(fd, &elf_header, sizeof(elf_header)) != sizeof(elf_header))
 	{
 		fprintf(stderr, "Error: Unable to read ELF header\n");
@@ -66,8 +67,11 @@ void display_elf_header(const Elf64_Ehdr *header)
 	}
 	printf("\n");
 
-	printf("  Class:%s\n", (header->e_ident[EI_CLASS] == ELFCLASS64) ? "ELF64" : "ELF32");
-	printf("  Data:%s\n", (header->e_ident[EI_DATA] == ELFDATA2LSB) ? "2's complement, little endian" : "2's complement, big endian");
+	printf("  Class:%s\n",
+			(header->e_ident[EI_CLASS] == ELFCLASS64) ? "ELF64" : "ELF32");
+	printf("  Data:%s\n",
+			(header->e_ident[EI_DATA] == ELFDATA2LSB) ?
+			"2's complement, little endian" : "2's complement, big endian");
 	printf("  Version:%d\n", header->e_ident[EI_VERSION]);
 	printf("  OS/ABI:%d\n", header->e_ident[EI_OSABI]);
 	printf("  ABI Version:%d\n", header->e_ident[EI_ABIVERSION]);
