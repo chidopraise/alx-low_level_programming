17694761b81c6763a0561875b68cd8c2f6128573
Abraham Livinus <abrahamlivinus@gmail.com>
2024-01-11 15:38:37 -0800

    5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -18,5 +18,5 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	for (i = 0; current != NULL && i < index; i++)
 		current = current->next;
 
-	return current;
+	return (current);
 }
