30a60e1dbc4e70a5b0a72f629a5a47a9b2d085e5
Abraham Livinus <abrahamlivinus@gmail.com>
2024-01-12 06:27:31 -0800

    103-keygen.c

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	char a[10],c[10];
+	int i,j,k=0;
+	printf("#Keygen by b44nz0r\n\n");
+
+	while (k <5 || k >=10)
+	{
+		if (k !=0)
+		{
+			printf("\nThe username length should be 5 to 10 alphabets\n");
+		}
+		else
+		{
+			printf("enter username: ");
+		}
+		scanf("%s",a);
+		k = strlen(a);
+	}
+
+	i = k-1;
+	j = 0;
+
+	while (i >= 0)
+	{
+		c[j] = a[i]+i;
+		i--;
+		j++;
+	}
+
+	c[j] = 0;
+	printf("\nThe password is %s\n",c);
+	printf("\nHit Enter to Exit\n");
+	getchar();
+	getchar();
+}
