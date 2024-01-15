2e7991994dc260dc434d2eeed8072e58c3f36e34
Abraham Livinus <abrahamlivinus@gmail.com>
2024-01-15 13:01:06 -0800

    shared library

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int add(int a, int b) {
+	return a + b;
+}
+
+int sub(int a, int b) {
+	return a - b;
+}
+
+int mul(int a, int b) {
+	return a * b;
+}
+
+int div(int a, int b) {
+	if (b != 0) {
+		return a / b;
+	} else {
+		printf("Error: Division by zero\n");
+		return 0;
+	}
+}
+
+int mod(int a, int b) {
+	if (b != 0) {
+		return a % b;
+	} else {
+		printf("Error: Modulo by zero\n");
+		return 0;
+	}
+}
