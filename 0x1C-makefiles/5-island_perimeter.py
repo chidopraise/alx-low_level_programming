3759f50db11401de8bb1f7cf2d0511e5eb1cc54f
Abraham Livinus <abrahamlivinus@gmail.com>
2024-03-20 16:35:21 -0700

    5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Defines an island perimeter measuring function."""
+
+
+def island_perimeter(grid):
+    """
+    Returns the perimeter of an island described in grid:
+        grid is a list of list of integers:
+            *0 represents a water zone
+            *1 represents a land zone
+            *One cell is a square with side length 1
+            *Grid cells are connected horizontally/vertically (not diagonally)
+            *Grid is rectangular, width and height don’t exceed 100
+            *Grid is completely surrounded by water, and there is
+                one island (or nothing)
+            *The island doesn’t have “lakes” (water inside that isn’t
+                connected to the water around the island)
+    Args(grid):
+                given list of a double array of 0s and 1s
+    Returns:
+            the preimeter of the island
+    """
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
